9f381e6e4e9ecf2636cb27cd3ab02f9379be536e
Alona <alonanepomnasa@gmail.com>
2026-03-07 13:20:38 +0200

    Add staircase solution

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,30 @@
-// https://www.hackerrank.com/challenges/staircase/problem
+pub fn staircase(n: usize) -> Vec<String> {
+    let mut result = Vec::new();
+
+    for i in 1..=n {
+        let spaces = " ".repeat(n - i);
+        let hashes = "#".repeat(i);
+        result.push(format!("{}{}", spaces, hashes));
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_staircase() {
+        let result = staircase(4);
+
+        let expected = vec![
+            "   #".to_string(),
+            "  ##".to_string(),
+            " ###".to_string(),
+            "####".to_string(),
+        ];
+
+        assert_eq!(result, expected);
+    }
+}
