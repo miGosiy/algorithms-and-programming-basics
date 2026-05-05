fb38ac741664d20088b87e9d14ae27943ae57eae
Alona <alonanepomnasa@gmail.com>
2026-05-05 17:50:51 +0300

    Практична робота 4: додано task04 з кодом

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,1 +1,51 @@
-// https://www.hackerrank.com/challenges/grading/problem
+/*
+Original solution (Python):
+
+s, t = map(int, input().split())
+a, b = map(int, input().split())
+m, n = map(int, input().split())
+
+apples = list(map(int, input().split()))
+oranges = list(map(int, input().split()))
+
+count_apples = 0
+for d in apples:
+    if s <= a + d <= t:
+        count_apples += 1
+
+count_oranges = 0
+for d in oranges:
+    if s <= b + d <= t:
+        count_oranges += 1
+
+print(count_apples)
+print(count_oranges)
+*/
+
+pub fn count_apples_and_oranges(
+    s: i32,
+    t: i32,
+    a: i32,
+    b: i32,
+    apples: &[i32],
+    oranges: &[i32],
+) -> (i32, i32) {
+    let mut count_apples = 0;
+    let mut count_oranges = 0;
+
+    for d in apples {
+        let pos = a + d;
+        if pos >= s && pos <= t {
+            count_apples += 1;
+        }
+    }
+
+    for d in oranges {
+        let pos = b + d;
+        if pos >= s && pos <= t {
+            count_oranges += 1;
+        }
+    }
+
+    (count_apples, count_oranges)
+}
