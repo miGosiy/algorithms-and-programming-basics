31df4f0849ebc06dd444a3271e2d47042d50895b
Alona <alonanepomnasa@gmail.com>
2026-05-05 18:14:03 +0300

    Практична робота 9: додано task09

--- src/hackerrank/task09.rs
+++ src/hackerrank/task09.rs
@@ -1,1 +1,19 @@
 // https://www.hackerrank.com/challenges/migratory-birds/problem
+/*
+n = int(input())
+arr = list(map(int, input().split()))
+
+count = {}
+pairs = 0
+
+for x in arr:
+    if x in count:
+        count[x] += 1
+    else:
+        count[x] = 1
+
+for value in count.values():
+    pairs += value // 2
+
+print(pairs)
+*/
