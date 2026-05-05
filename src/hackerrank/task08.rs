2cf3b09a7837002842bf5a078c773eae46c7d9f7
Alona <alonanepomnasa@gmail.com>
2026-05-05 18:12:12 +0300

    Практична робота 8: додано task08

--- src/hackerrank/task08.rs
+++ src/hackerrank/task08.rs
@@ -1,1 +1,16 @@
 // https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+/*
+n = int(input())
+arr = list(map(int, input().split()))
+count = [0] * 6
+
+for x in arr:
+    count[x] += 1
+
+max_count = max(count)
+
+for i in range(1, 6):
+    if count[i] == max_count:
+        print(i)
+        break
+*/
