0d554d7c151e990a554ba609cd5bbdd064f2f5c4
Alona <alonanepomnasa@gmail.com>
2026-05-05 18:10:53 +0300

    Практична робота 7: додано task07

--- src/hackerrank/task07.rs
+++ src/hackerrank/task07.rs
@@ -1,1 +1,20 @@
 // https://www.hackerrank.com/challenges/between-two-sets/problem
+/*
+n = int(input())
+scores = list(map(int, input().split()))
+
+max_score = scores[0]
+min_score = scores[0]
+max_count = 0
+min_count = 0
+
+for score in scores[1:]:
+    if score > max_score:
+        max_score = score
+        max_count += 1
+    elif score < min_score:
+        min_score = score
+        min_count += 1
+
+print(max_count, min_count)
+*/
