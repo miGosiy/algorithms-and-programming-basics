792c26e5089999c3e94b22f4391540679e065ba2
Alona <alonanepomnasa@gmail.com>
2026-05-29 19:45:49 +0300

    Add task08 solution and results

--- src/hackerrank/task08.rs
+++ src/hackerrank/task08.rs
@@ -1,16 +1,30 @@
-// https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
-/*
-n = int(input())
-arr = list(map(int, input().split()))
-count = [0] * 6
+// https://www.hackerrank.com/challenges/migratory-birds/problem
 
-for x in arr:
-    count[x] += 1
+pub fn migratory_birds(arr: Vec<i32>) -> i32 {
+    let mut count = vec![0; 6];
 
-max_count = max(count)
+    for x in arr {
+        count[x as usize] += 1;
+    }
 
-for i in range(1, 6):
-    if count[i] == max_count:
-        print(i)
-        break
-*/
+    let max_count = *count.iter().max().unwrap();
+
+    for i in 1..=5 {
+        if count[i] == max_count {
+            return i as i32;
+        }
+    }
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_migratory_birds() {
+        let arr = vec![1, 4, 4, 4, 5, 3];
+        assert_eq!(migratory_birds(arr), 4);
+    }
+}
