4209da2d26e74b04e330c14f300a53361e519abd
Alona <alonanepomnasa@gmail.com>
2026-05-29 19:49:02 +0300

    Add task04 solution and results

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,27 +1,3 @@
-/*
-Original solution (Python):
-
-s, t = map(int, input().split())
-a, b = map(int, input().split())
-m, n = map(int, input().split())
-
-apples = list(map(int, input().split()))
-oranges = list(map(int, input().split()))
-
-count_apples = 0
-for d in apples:
-    if s <= a + d <= t:
-        count_apples += 1
-
-count_oranges = 0
-for d in oranges:
-    if s <= b + d <= t:
-        count_oranges += 1
-
-print(count_apples)
-print(count_oranges)
-*/
-
 pub fn count_apples_and_oranges(
     s: i32,
     t: i32,
