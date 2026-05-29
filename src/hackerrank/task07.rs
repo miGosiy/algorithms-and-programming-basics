2fcf01d434bb9f4dfe2e5d72014fc981eadde81b
Alona <alonanepomnasa@gmail.com>
2026-05-29 19:52:27 +0300

    Add task07 solution and results

--- src/hackerrank/task07.rs
+++ src/hackerrank/task07.rs
@@ -1,20 +1,31 @@
-// https://www.hackerrank.com/challenges/between-two-sets/problem
-/*
-n = int(input())
-scores = list(map(int, input().split()))
+// https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
 
-max_score = scores[0]
-min_score = scores[0]
-max_count = 0
-min_count = 0
+pub fn breaking_records(scores: Vec<i32>) -> Vec<i32> {
+    let mut max_score = scores[0];
+    let mut min_score = scores[0];
+    let mut max_count = 0;
+    let mut min_count = 0;
 
-for score in scores[1:]:
-    if score > max_score:
-        max_score = score
-        max_count += 1
-    elif score < min_score:
-        min_score = score
-        min_count += 1
+    for &score in scores.iter().skip(1) {
+        if score > max_score {
+            max_score = score;
+            max_count += 1;
+        } else if score < min_score {
+            min_score = score;
+            min_count += 1;
+        }
+    }
 
-print(max_count, min_count)
-*/
+    vec![max_count, min_count]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_breaking_records() {
+        let scores = vec![10, 5, 20, 20, 4, 5, 2, 25, 1];
+        assert_eq!(breaking_records(scores), vec![2, 4]);
+    }
+}
